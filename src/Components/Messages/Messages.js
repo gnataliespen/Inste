ef67a3fe04592ba84c8174062a765981c7bcdbbd
Natalie Spencer <gnataliespen@gmail.com>
2019-11-19 02:37:53 -0600

    Added messages display
    Messages now display in room, currently just displaying dummy data, will soon set up backend to send messages for room.

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import { Segment, Comment } from "semantic-ui-react";
 
 import firebase from "../../firebase";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import Message from "./Message";
 
 const Messages = ({ currentChannel, currentUser }) => {
+  const [messages, setMessages] = useState([
+    {
+      content: "adsda",
+      timestamp: 1574149154470,
+      user: {
+        avatar:
+          "http://gravatar.com/avatar/a13cf70aacb36fd2595f62ef51a3d02f?d=identicon",
+        id: "1vD5SKOtNFUKTHFDNoWVAKZQ5Km1",
+        name: "gnatalie"
+      }
+    },
+    {
+      content: "dadas",
+      timestamp: 1574149154480,
+      user: {
+        avatar:
+          "http://gravatar.com/avatar/a13cf70aacb36fd2595f62ef51a3d02f?d=identicon",
+        id: "1vD5SKOtNFUKTHFDNoWVAKZQ5Km1",
+        name: "gnatalie"
+      }
+    },
+    {
+      content: "adsssssda",
+      timestamp: 1574149154460,
+      user: {
+        avatar:
+          "http://gravatar.com/avatar/a13cf70aacb36fd2595f62ef51a3d02f?d=identicon",
+        id: "1vD5SKOtNFUKTHFDNoWVAKZQ5Km1",
+        name: "gnatdsdsalie"
+      }
+    },
+    {
+      content: "adsdadsaa",
+      timestamp: 1574149154470,
+      user: {
+        avatar:
+          "http://gravatar.com/avatar/a13cf70aacb36fd2595f62ef51a3d02f?d=identicon",
+        id: "1vD5SKOtNFUKTHFDNoWVAKZQ5Km1",
+        name: "gnaaatalie"
+      }
+    }
+  ]);
+
   const messagesRef = firebase.database().ref("messages");
+
+  const displayMessages = msgs =>
+    msgs.length > 0 &&
+    msgs.map(msg => (
+      <Message key={msg.timestamp} message={msg} user={currentUser} />
+    ));
 
   return (
     <React.Fragment>
       <MessagesHeader />
 
       <Segment>
-        <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        <Comment.Group className="messages">
+          {displayMessages(messages)}
+        </Comment.Group>
       </Segment>
 
       <MessageForm
